82d2d0bdbdb7b6759f63ecc4a12fbe9bbe72f6bf
avi-anviam <avirajdhooria2001@gmail.com>
2022-10-17 13:41:37 -0700

    Final Edit to convert if/else statements to ternary operators
    - Replaced if/else statements with ternary and nested ternary operators.
    - Added in-line comments for a better understanding of the code.

--- routes/api/v1/apiv1.js
+++ routes/api/v1/apiv1.js
@@ -13,33 +13,33 @@ router.get('/urls/preview', function(req, res, next) {
     fetch(queryString)
         .then(res => res.text())
         .then(value => {
+            //Set the type to HTML.
             res.type('html');
+            //Parse response's value to get the html.
             const parsedHTML = parser.parse(value);
+            /*
+                ** Creating variables for the url, title, image, desc, and type from open
+                ** graph meta tags.
+            */
             const openGraphURL = parsedHTML.querySelector("meta[property='og:url']");
             const openGraphTitle = parsedHTML.querySelector("meta[property='og:title']");
             const openGraphImage = parsedHTML.querySelector("meta[property='og:image']");
             const openGraphDescription = parsedHTML.querySelector("meta[property='og:description']");
             const openGraphType = parsedHTML.querySelector("meta[property='og:type']");
+
+            //Setting up values for the preview features and creating HTML elements
             let url = openGraphURL == null ? queryString : openGraphURL.getAttribute('content');
-            let title = "";
-            let image = "";
-            let type = "";
-            if(openGraphImage != null) {
-                image = '<img src='+openGraphImage.getAttribute('content') +'  style="max-height: 200px; max-width: 270px;">';
-            }
+            let title = openGraphTitle == null ? 
+                        (parsedHTML.getElementsByTagName('title')[0] != undefined ?
+                         parsedHTML.getElementsByTagName('title')[0].innerHTML :
+                         queryString) : openGraphTitle.getAttribute('content');
+            let image = openGraphImage == null ? 
+                        "" : '<img src='+openGraphImage.getAttribute('content') +'  style="max-height: 200px; max-width: 270px;">';
+            let type = openGraphType == null ? 
+                        "" : '<p style="text-decoration: underline;">Content: ' + openGraphType.getAttribute('content') + '</p>';
             let description = openGraphDescription == null ? "" : "<p>" + openGraphDescription.getAttribute('content') + "</p>";
-            if(openGraphTitle == null) {
-                if(parsedHTML.getElementsByTagName('title')[0] != undefined) {
-                    title = parsedHTML.getElementsByTagName('title')[0].innerHTML;
-                } else {
-                    title = queryString;
-                }
-            } else {
-                title = openGraphTitle.getAttribute('content');
-            }
-            if(openGraphType != null) {
-                type = '<p style="text-decoration: underline;">Content: ' + openGraphType.getAttribute('content') + '</p>';
-            }
+
+            //The HTML response that the API sends
             const htmlResponse = 
                 `<div style="max-width: 300px; border: solid 1px; padding: 3px; text-align: center;">
                     <a href=${url}>
