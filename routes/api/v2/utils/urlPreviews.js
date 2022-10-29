e9198f8774b8b1fab43bdbb5a7a46cca1859f525
avi-anviam <avirajdhooria2001@gmail.com>
2022-10-29 01:56:57 -0700

    Deployed to azure and fixed XSS attacks

--- routes/api/v2/utils/urlPreviews.js
+++ routes/api/v2/utils/urlPreviews.js
@@ -3,6 +3,14 @@ import fetch from 'node-fetch';
 import parser from 'node-html-parser';
 
 async function getURLPreview(url){
+  const escapeHTML = str => str.replace(/[&<>'"]/g, 
+  tag => ({
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      "'": '&#39;',
+      '"': '&quot;'
+    }[tag]));
   let content = fetch(url)
         .then(res => res.text())
         .then(value => {
@@ -12,16 +20,16 @@ async function getURLPreview(url){
             const openGraphImage = parsedHTML.querySelector("meta[property='og:image']");
             const openGraphDescription = parsedHTML.querySelector("meta[property='og:description']");
             const openGraphType = parsedHTML.querySelector("meta[property='og:type']");
-            let previewURL = openGraphURL == null ? url : openGraphURL.getAttribute('content');
+            let previewURL = openGraphURL == null ? url : escapeHTML(openGraphURL.getAttribute('content'));
             let title = openGraphTitle == null ? 
                         (parsedHTML.getElementsByTagName('title')[0] != undefined ?
-                         parsedHTML.getElementsByTagName('title')[0].innerHTML :
+                         escapeHTML(parsedHTML.getElementsByTagName('title')[0].innerHTML) :
                          url) : openGraphTitle.getAttribute('content');
             let image = openGraphImage == null ? 
-                        "" : '<img src='+openGraphImage.getAttribute('content') +'  style="max-height: 200px; max-width: 270px;">';
+                        "" : '<img src='+ escapeHTML(openGraphImage.getAttribute('content')) +'  style="max-height: 200px; max-width: 270px;">';
             let type = openGraphType == null ? 
-                        "" : '<p style="text-decoration: underline;">Content: ' + openGraphType.getAttribute('content') + '</p>';
-            let description = openGraphDescription == null ? "" : "<p>" + openGraphDescription.getAttribute('content') + "</p>";
+                        "" : '<p style="text-decoration: underline;">Content: ' + escapeHTML(openGraphType.getAttribute('content')) + '</p>';
+            let description = openGraphDescription == null ? "" : "<p>" + escapeHTML(openGraphDescription.getAttribute('content')) + "</p>";
             const htmlResponse = 
                 `<div style="max-width: 300px; border: solid 1px; padding: 3px; text-align: center;">
                     <a href=${previewURL}>
